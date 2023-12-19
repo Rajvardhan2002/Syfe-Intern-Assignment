4162cc1cb81fe928b72b15f4c9e819531d24bdb9
Rajvardhan Singh <rajwardhansinghrajput@gmail.com>
2023-12-19 21:55:45 +0530

    added model

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const path = require("path");
+const db = require("./data/database");
 
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+
 app.use(express.static("public"));
+app.use(express.urlencoded({extended:false}));
 
 const homeRoutes = require("./routes/home.routes");
 const authRoutes = require("./routes/auth.routes");
@@ -13,4 +16,12 @@ const authRoutes = require("./routes/auth.routes");
 app.use(authRoutes);
 app.use(homeRoutes);
 
-app.listen(3000,()=>{console.log("Server started on port 3000");});
+// app.listen(3000,()=>{console.log("Server started on port 3000");});
+db.connectToDatabase()
+  .then(function () {
+    app.listen(3000);
+  })
+  .catch(function (error) {
+    console.log("Failed to connect to the database!");
+    console.log(error);
+  });
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,30 @@
-function getSignup(req,res){
-    res.render("auth/signup");
+const User = require("../models/user.model");
+
+function getSignup(req, res) {
+  res.render("auth/signup");
 }
 
-function getLogin(req,res){
-    res.render("auth/login");
+////form submission
+async function signup(req, res) {
+  const user = new User(
+    req.body.email,
+    req.body.password,
+    req.body.name,
+    req.body.account,
+    req.body.ifsc
+  );
+  await user.signup();
+  res.redirect("/login");
+}
+
+
+////shows login page
+function getLogin(req, res) {
+  res.render("auth/login");
 }
 
 module.exports = {
-    getSignup : getSignup,
-    getLogin : getLogin,
-}
+  getSignup: getSignup,
+  signup: signup,
+  getLogin: getLogin,
+};

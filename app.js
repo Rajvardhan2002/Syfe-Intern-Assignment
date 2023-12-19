bb0d98e14dc4fa4b1b0f042196befa7039139ae9
Rajvardhan Singh <rajwardhansinghrajput@gmail.com>
2023-12-19 19:58:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
+
+const homeRoutes = require("./routes/home.routes");
+const authRoutes = require("./routes/auth.routes");
+
+app.use(authRoutes);
+app.use(homeRoutes);
+
+app.listen(3000);
---
+++ routes/home.routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const homeController = require("../controllers/home.controller");
+
+const router = express.Router();
+
+//shows account description of users
+router.get("/details", homeController.getDetails); 
+
+///shows prediction form
+router.get("/predictTransactions",homeController.getPrediction);
+
+router.get("/detail-transactions" , homeController.getTransactions);
+
+
+module.exports = router;

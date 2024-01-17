1abb56517f9d8a272d42f33811d8796d8d34ed5d
Rajvardhan Singh <rajwardhansinghrajput@gmail.com>
2024-01-17 18:01:15 +0530

    raj changes

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -2,20 +2,25 @@ const mongodb = require("mongodb");
 const db = require("../data/database");
 const ObjectId = mongodb.ObjectId;
 
-async function getDetails(req, res) {
-  const receivedData = await db
-    .getDb()
-    .collection("users")
-    .findOne({ _id: new ObjectId(req.session.uid) });
-  const tableArray = await db
-    .getDb()
-    .collection("userTransactions")
-    .find({ userID: req.session.uid })
-    .sort({ dateSort: -1 })
-    .toArray();
+async function getDetails(req, res, next) {
+  try {
+    const receivedData = await db
+      .getDb()
+      .collection("users")
+      .findOne({ _id: new ObjectId(req.session.uid) });
+    const tableArray = await db
+      .getDb()
+      .collection("userTransactions")
+      .find({ userID: req.session.uid })
+      .sort({ dateSort: -1 })
+      .toArray();
 
-  // console.log(tableArray);
-  res.render("index/details", { user: receivedData, tableArray: tableArray });
+    // console.log(tableArray);
+    res.render("index/details", { user: receivedData, tableArray: tableArray });
+  } catch (error) {
+    next(error);
+    return;
+  }
 }
 
 module.exports = {
